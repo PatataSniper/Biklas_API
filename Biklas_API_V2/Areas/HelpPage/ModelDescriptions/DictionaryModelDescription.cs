c2b8b8de98b7ec2789173411883d860b135c7f44
Saúl Muñoz García <saul.mgarcia@alumnos.udg.mx>
2021-07-18 14:58:41 -0500

    Agregar archivos de proyecto.

---
+++ Biklas_API_V2/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Biklas_API_V2.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ Biklas_API_V2/Areas/HelpPage/ModelDescriptions/EnumValueDescription.cs
@@ -0,0 +1,11 @@
+namespace Biklas_API_V2.Areas.HelpPage.ModelDescriptions
+{
+    public class EnumValueDescription
+    {
+        public string Documentation { get; set; }
+
+        public string Name { get; set; }
+
+        public string Value { get; set; }
+    }
+}
