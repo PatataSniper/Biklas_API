283130a50cbfe78d009207ad31493ed6034526aa
Saúl Muñoz García <saul.mgarcia@alumnos.udg.mx>
2021-12-08 14:34:10 -0600

    Configuración de login y pruebas

--- Biklas_API_V2/SQL/Pruebas.sql
+++ Biklas_API_V2/SQL/Pruebas.sql
@@ -1,9 +1,19 @@
 select * from Usuarios
+
+-- Creamos nuevo usuario
 INSERT INTO Usuarios (IdUsuario, Nombre, Contraseña, KmRecorridos,
 	Apellidos, NombreUsuario, FechaNacimiento, FechaRegistro,
 	CorreoElectronico, IdRol)
-	VALUES (2, 'Carlos', 'Contra2', 0, 'Arias', 'CaA', '1996-06-12',
-	'2021-08-08', 'carlos@alumnos.udg.mx', 1)
+	VALUES (3, 'Andres', 'Contra3', 568, 'De la peña', 'Sgt0pimienta', '1999-11-23',
+	'2020-05-18', 'andres@gmail.com', 1)
+
+select * from Amigos
+-- Creamos relaciones de amigos entre usuarios, una relación de amistad debe
+-- de crearse en ambos sentidos
+INSERT INTO Amigos (IdUsuario, IdAmigo, FechaRelacion)
+	VALUES (1, 3, '2021-11-24')
+INSERT INTO Amigos (IdUsuario, IdAmigo, FechaRelacion)
+	VALUES (3, 1, '2021-11-24')
 
 INSERT INTO Roles (IdRol, Nombre)
 	VALUES (1, 'Administrador')
